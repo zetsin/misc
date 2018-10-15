380614162211f554819d76734d5f8ea7f5c6fccb
Zetsin <zetsin@gmail.com>
2018-10-15 18:03:22 +0800

    Update evaluate.js

--- evaluate.js
+++ evaluate.js
@@ -1,6 +1,6 @@
 (function(){
   var obj = document.createElement('div')
-  obj.style.cssText = 'position:fixed;top:300px;left:300px;width:200px;height:200px;background:red;'
+  obj.style.cssText = 'position:fixed;top:100px;left:100px;width:200px;height:200px;background:red;'
   document.body.appendChild(obj)
 })()
   
