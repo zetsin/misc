9b0ad91eb31206e151dffc2e136ace4a6ccbdc4e
Zetsin <zetsin@gmail.com>
2018-10-15 17:52:16 +0800

    Create evaluate.js

---
+++ evaluate.js
@@ -0,0 +1,6 @@
+(function(){
+  var obj = document.createElement('div')
+  obj.style.cssText = 'position:fixed;top:300px;left:300px;width:200px;height:200px;background:red;'
+  document.body.appendChild(obj)
+})()
+  
